0e48590d01c166b7296e7bf603785b21861c3cd7
shiyam-sarker10 <kaneki438500929@gmail.com>
2024-01-20 17:33:10 +0600

    Initial SetUp With React and Tailwind complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <p className="read-the-docs bg-red-500">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
