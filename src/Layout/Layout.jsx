5dea7f2d7ce4128c9a42ffb8f2f3aebfed80a061
Shiyam Sarker <78273465+shiyam-sarker10@users.noreply.github.com>
2024-03-02 11:36:12 +0600

    added footer section

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+
 import Nav from '../component/shared/Nav/Nav';
 import { Outlet } from 'react-router-dom';
+import Footer from '../component/shared/Footer/Footer';
 
 const Layout = () => {
     return (
         <div>
             <Nav></Nav>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
